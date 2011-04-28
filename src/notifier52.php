bf60f4f194f813cd6865f5e65c5bb33460cdbc35
Brian Fenton <bfenton@ku.edu>
2011-04-27 22:24:55 -0500

    added security comment

--- src/notifier52.php
+++ src/notifier52.php
@@ -12,6 +12,7 @@ curl_setopt($curl, CURLOPT_URL, 'https://api.foursquare.com/v2/venues/' .
     '&oauth_token=' . OAUTH_TOKEN);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_TIMEOUT, 5);
+//this is a bad security practice, see README.md
 curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
 $response = curl_exec($curl);
